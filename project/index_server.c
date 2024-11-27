1cc0982164325f0bc3daeee3940b8dcee212fbf5
awaisirshad1 <awaisirshad1@icloud.com>
2024-11-27 09:59:18 -0500

    continuing project

--- project/index_server.c
+++ project/index_server.c
@@ -6,36 +6,326 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <time.h>
-#include "libr.h"
+#include <arpa/inet.h>
+
+
 #define FILE_NAME_MAX 20
 #define UPD_MAX_BUF 100
+#define MAX_LEDGER_ENTRIES 200
 #define NOT_FOUND "Content Not Found"
-#define MAX_LEDGER_ENTRIES 50
 
-typedef truct ledger_entry{
-    char peerName[FILE_NAME_MAX];
+
+typedef struct ledger_entry 
+{
+    char content_host[FILE_NAME_MAX];
     char ip_addr[16];
     char port_num[7];
-    int token;
+    short token;
     struct ledger_entry * next;
-} LEDGER_ENTRY;
+}   LEDGER_ENTRY;
 
-typedef srtuct { 
-    char 
-} LEDGER;
-struct IndexLedgerEntry IndexLedger[NUM_PEERS];
+typedef struct 
+{
+    char content_name[FILE_NAME_MAX]; // Content Name
+    LEDGER_ENTRY * head;
+}   LEDGER;
+    LEDGER list[MAX_LEDGER_ENTRIES];
 
-char *contentLedger[];
-// index server needs to constantly listen for peers requests to connect to another peer over   
-void registerPeer(){
-    struct pdu request; 
-	struct pdu response;
-    struct sockaddr_in client_addr;	/* the from address of a client	*/
-}
+int max_index = 0;
 
-int main(void){
-    int socket desc;
-    struct sockaddr_in server_address, client_address;
-    
-    return (0);
+// Pdu for udp communication
+typedef struct 
+{
+    char type;
+    char data[UPD_MAX_BUF];
+}   PDU_UDP;
+PDU_UDP tpdu;
+
+// METHOD SIGNATURES
+// find content by name
+void search_content(int, char * , struct sockaddr_in * );
+// register a peer as content server
+void registration(int, char * , struct sockaddr_in * );
+// de-register a peer
+void deregistration(int, char * , struct sockaddr_in * );
+
+struct sockaddr_in fsin;
+
+// UDP Content Indexing Service
+int main(int argc, char * argv[]) {
+    struct sockaddr_in sin, * p_addr; // the from address of a client	
+    LEDGER_ENTRY * p_entry;
+    char * service = "10000"; // service content_name or port_num number	
+    char content_name[FILE_NAME_MAX], content_host[FILE_NAME_MAX];
+    int alen = sizeof(struct sockaddr_in); // from-address length		
+    int s, n, i, len, p_sock; // socket descriptor and socket type    
+    int pdulen = sizeof(PDU_UDP);
+    struct hostent * hp;
+    PDU_UDP udp_pdu;
+    int j = 0;
+
+    PDU_UDP spdu;
+
+    for (n = 0; n < MAX_LEDGER_ENTRIES; n++)
+        list[n].head = NULL;
+    switch (argc) {
+    case 1:
+        break;
+    case 2:
+        service = argv[1];
+        break;
+    default:
+        fprintf(stderr, "Incorrect Arguments \n Use the format: server [host] [port_num]\n");
+    }
+
+    memset( & sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+
+    // Map service content_name to port_num number 
+    sin.sin_port = htons((u_short) atoi(service));
+
+    // Allocate socket 
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0) {
+        fprintf(stderr, "can't creat socket\n");
+        exit(1);
+    }
+
+    // Bind socket 
+    if (bind(s, (struct sockaddr * ) & sin, sizeof(sin)) < 0)
+        fprintf(stderr, "can't bind to %s port_num\n", service);
+    // receive UDP connections
+    while (1) {
+        if ((n = recvfrom(s, & udp_pdu, pdulen, 0, (struct sockaddr * ) & fsin, & alen)) < 0) {
+        printf("recvfrom error: n=%d\n", n);
+        }
+        //Content Registration Request 'R'			
+        if (udp_pdu.type == 'R') {
+        printf("Registering\n");
+        registration(s, udp_pdu.data, & fsin);
+        printf("%d\n", s);
+        }
+
+        // Search Content 'S'		
+        if (udp_pdu.type == 'S') {
+        printf("Searching\n");
+        search_content(s, udp_pdu.data, & fsin);
+        }
+
+        //List Content 'O' 
+        if (udp_pdu.type == 'O') {
+        printf("List content\n");
+        // Read from the content list and send the list to the client 		
+        for (j = 0; j < max_index; j++) {
+            if (list[j].head != NULL) {
+            PDU_UDP spdu;
+            spdu.type = 'O';
+            strcpy(spdu.data, list[j].content_name);
+            printf("%s\n", list[j].content_name);
+            (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+            }
+        }
+        // Acknowledge End of Response
+        PDU_UDP opdu;
+        opdu.type = 'A';
+        (void) sendto(s, & opdu, sizeof(opdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+        }
+
+        //Deregister 'T'		
+        if (udp_pdu.type == 'T') {
+        printf("de-register\n");
+        deregistration(s, udp_pdu.data, & fsin);
+        }
+    }
+    return;
+    }
+
+    // s is socket address
+    // *data is the data content_name
+    // *addr is the address of the client sending/requesting
+    void search_content(int s, char * data, struct sockaddr_in * addr) {
+    // Search content list and return the answer:
+    int j;
+    int found = 0;
+    int used = 999;
+    LEDGER_ENTRY * use;
+    LEDGER_ENTRY * head;
+    int pdulen = sizeof(PDU_UDP);
+    PDU_UDP spdu;
+    char content_host[20];
+    char ouput[100];
+    char fileName[20];
+    char rep[2] = ",";
+
+    strcpy(content_host, strtok(data, rep));
+    strcpy(fileName, strtok(NULL, rep));
+
+    // loop through list until you find content_name == data.
+    for (j = 0; j < max_index; j++) {
+        // check if the value at the list index 
+        // is equal to the requested file
+        printf("%s\n", list[j].content_name);
+        if (strcmp(list[j].content_name, fileName) == 0 && (list[j].head != NULL)) {
+        found = 1;
+        head = list[j].head;
+        // loop through the linked list until you find content_name
+        while (head != NULL) {
+            if (head -> token < used) {
+            used = head -> token;
+            use = head;
+            }
+            head = head -> next;
+        }
+        break;
+        }
+    }
+    if (found == 1) {
+        spdu.type = 'S';
+        strcpy(ouput, use -> content_host);
+        strcat(ouput, ",");
+        strcat(ouput, fileName);
+        strcat(ouput, ",");
+        strcat(ouput, use -> ip_addr);
+        strcat(ouput, ",");
+        strcat(ouput, use -> port_num);
+        printf("%s\n", ouput);
+        strcpy(spdu.data, ouput);
+        use -> token++;
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+    } else {
+        spdu.type = 'E';
+        strcpy(spdu.data, "File not found");
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+    }
+    printf("Ending\n");
+    }
+
+    // Deregister content from the server
+    void deregistration(int s, char * data, struct sockaddr_in * addr) {
+    int j;
+    int use = -1;
+    LEDGER_ENTRY * prev;
+    LEDGER_ENTRY * head;
+    int listIndex = 0;
+    PDU_UDP spdu;
+    char rep[2] = ",";
+    char content_host[20];
+    char file[20];
+    printf("Deregistering %s\n", data);
+    strcpy(content_host, strtok(data, rep));
+    strcpy(file, strtok(NULL, rep));
+    for (j = 0; j < max_index; j++) {
+        if (strcmp(list[j].content_name, file) == 0) {
+        head = list[j].head;
+        prev = list[j].head;
+        listIndex = 0;
+        while (head != NULL) {
+            printf("Usr list = %s\n", head -> content_host);
+            printf("Usr given = %s\n", content_host);
+            if (strcmp(head -> content_host, content_host) == 0) {
+            printf("Compare content_name success\n");
+            printf("List index = %d\n", listIndex);
+            use = listIndex;
+            if (listIndex == 0) {
+                list[j].head = head -> next;
+            } else {
+                prev -> next = head -> next;
+            }
+            break;
+            }
+            listIndex++;
+            prev = head;
+            head = head -> next;
+        }
+        break;
+        }
+    }
+    if (use != -1) {
+        spdu.type = 'A';
+        strcpy(spdu.data, "Done");
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+    } else {
+        spdu.type = 'E';
+        strcpy(spdu.data, "Failed");
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+    }
+    }
+
+    // s is socket address
+    // *data is the data content_name
+    // *addr is the address of the client sending/requesting
+    // Register Content to the Server
+    void registration(int s, char * data, struct sockaddr_in * addr) {
+    // Register the content and the server of the content
+    LEDGER_ENTRY * new = NULL;
+    new = (LEDGER_ENTRY * ) malloc(sizeof(LEDGER_ENTRY));
+    int j;
+    LEDGER_ENTRY * head;
+    int used = 999;
+
+    int duplicateUser = 0;
+    char rep[2] = ",";
+    PDU_UDP spdu;
+    char fileName[20];
+    int found = 0;
+    char * ip_addr = inet_ntoa(fsin.sin_addr);
+
+    printf("Sending ip_addr address %s\n", ip_addr);
+    printf("Socket %d\n", s);
+    printf("Data %s\n", data);
+    // get address and username from data ledger_entry
+    strcpy(new -> content_host, strtok(data, rep));
+    strcpy(fileName, strtok(NULL, rep));
+    strcpy(new -> port_num, strtok(NULL, rep));
+    strcpy(new -> ip_addr, ip_addr);
+
+    printf("Stored Ip %s\n", new -> ip_addr);
+    new -> token = 0;
+    new -> next = NULL;
+    // iterate list until you find content_name == data
+    for (j = 0; j < max_index; j++) {
+        if (strcmp(list[j].content_name, fileName) == 0) {
+        head = list[j].head;
+        found = 1;
+        // loop through the linked list until you get to the end to add content to tail
+        while (head != NULL) {
+            // stops seg faults when registering content after full deregister
+            if (head -> next == NULL) {
+            break;
+            }
+            // check if username is already used
+            if (strcmp(head -> content_host, data) == 0) {
+            duplicateUser = 1;
+            break;
+            }
+            head = head -> next;
+        }
+        if (head == NULL) { // First Content on Linked List Initialize
+            list[j].head = new;
+        } else { // Other Cases
+            head -> next = new;
+        }
+        break;
+        }
+    }
+    //File not Found
+    if (found == 0) {
+        strcpy(list[max_index].content_name, fileName);
+        list[max_index].head = new;
+        max_index++;
+    }
+    if (duplicateUser == 1) {
+        printf("Duplicate\n");
+        spdu.type = 'E';
+        strcpy(spdu.data, "Duplicate user content_name"); // Duplicate File Name?
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+
+    } else {
+        printf("Unique\n");
+        // Otherwise send acknowledgement 'A'
+        spdu.type = 'A';
+        strcpy(spdu.data, "Done");
+        (void) sendto(s, & spdu, sizeof(spdu), 0, (struct sockaddr * ) & fsin, sizeof(fsin));
+    }
 }
--- project/peer.c
+++ project/peer.c
@@ -1,26 +1,277 @@
+#include <stdio.h>
+#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <time.h>
-#include "libr.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <strings.h>
+#include <sys/wait.h>
+#include <sys/signal.h>
 
-// name of this peer
-static const char* PEER_NAME;
+// Define constants for quit message, server port, connection limits, and buffer sizes
+#define QUIT "quit"
+#define SERVER_PORT 10000
+#define CONNECTION_REQUEST_LIMIT 5
+#define BUFFER_LENGTH 100
+#define NAME_SIZE 20
+#define MAX_CONNECTIONS 200
 
-int tcpPassiveSocketNum;
+// Define the PDU (Protocol Data Unit) structures for UDP packets and size-based PDUs
+typedef struct {
+    char messageType;            // Message type (e.g., R, T, L)
+    char messageData[BUFFER_LENGTH]; // Data field for the UDP packet
+} PDU_UDP;
 
+typedef struct {
+    char messageType;              // Message type (e.g., R, T, L)
+    char messageData[BUFFER_LENGTH]; // Data field for the UDP packet
+    int dataSize;                   // Size of the data in the packet
+} PDU_WithSize;
 
-void registerThisPeer(){
-    struct sockaddr_in reg_addr;
-    s = socket(AF_INET, SOCK_STREAM,0);
-    reg_addr.sin_family 
+// This structure tracks the list of registered files on the server
+PDU_UDP registrationPDU;
+
+// Structure to track files and their status
+struct {
+    int fileStatus;             // Status of the file (1 - registered, 0 - deregistered)
+    char fileName[NAME_SIZE];   // Name of the file
+} registeredFileTable[MAX_CONNECTIONS]; // Table to store registered files
+
+// Global variables for user and socket information
+char userName[NAME_SIZE];
+int serverSocket, peerPort;
+int tcpSocket, numberOfFileDescriptors;
+fd_set readFileDescriptors, allFileDescriptors;
+
+void registerContent(int, char * );
+int searchForContent(int, char * , PDU_UDP * );
+int downloadContentFromClient(char * , PDU_UDP * );
+int handleServerDownload(int);
+void deregisterContent(int, char * );
+void listOnlineUsers(int);
+void listLocalContent();
+void exitProgram(int);
+void signalHandler();
+void childProcessReaper(int);
+int currentIndex = 0;
+char peerUserName[10];
+int processId;
+
+int main(int argc, char ** argv) {
+    int serverPort = SERVER_PORT;
+    int bytesRead;
+    int addressLength = sizeof(struct sockaddr_in);
+    struct hostent * hostEntry;
+    struct sockaddr_in serverAddress;
+    char commandCharacter, * host, peerName[NAME_SIZE];
+    struct sigaction signalAction;
+    char dataToSend[100];
+
+    // Parse command-line arguments for host and port
+    switch (argc) {
+    case 2:
+        host = argv[1];
+        break;
+    case 3:
+        host = argv[1];
+        serverPort = atoi(argv[2]);
+        break;
+    default:
+        printf("Usage: %s host [port]\n", argv[0]);
+        exit(1);
+    }
+
+    // Create a TCP socket for communication with the client server
+    int tcpSocketDescriptor, newSocketDescriptor, clientAddressLength, portNumber, m;
+    struct sockaddr_in clientAddress, clientAddress2;
+
+    if ((tcpSocketDescriptor = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        fprintf(stderr, "Unable to create a socket\n");
+        exit(1);
+    }
+
+    char localIP[16];
+    bzero((char * ) & clientAddress, sizeof(struct sockaddr_in));
+    socklen_t clientAddressLength = sizeof(clientAddress);
+
+    if (bind(tcpSocketDescriptor, (struct sockaddr * ) & clientAddress, sizeof(clientAddress)) == -1) {
+        fprintf(stderr, "Unable to bind address to socket\n");
+        exit(1);
+    }
+
+    // Start listening for incoming connection requests
+    listen(tcpSocketDescriptor, CONNECTION_REQUEST_LIMIT);
+
+    // Set up signal handler for child processes
+    (void) signal(SIGCHLD, childProcessReaper);
+
+    // Set up UDP communication with the server
+    memset(&serverAddress, 0, addressLength);
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(serverPort);
+
+    if (hostEntry = gethostbyname(host))
+        memcpy(&serverAddress.sin_addr, hostEntry->h_addr, hostEntry->h_length);
+    else if ((serverAddress.sin_addr.s_addr = inet_addr(host)) == INADDR_NONE) {
+        printf("Unable to get host entry\n");
+        exit(1);
+    }
+
+    // Create a UDP socket for communication with the index server
+    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (serverSocket < 0) {
+        printf("Unable to create socket\n");
+        exit(1);
+    }
+
+    // Connect to the index server using the UDP socket
+    if (connect(serverSocket, (struct sockaddr * ) & serverAddress, sizeof(serverAddress)) < 0) {
+        printf("Unable to connect to the server\n");
+        exit(1);
+    }
+
+    // Get the port number the client has opened and display the local IP address
+    getsockname(tcpSocketDescriptor, (struct sockaddr * ) & clientAddress, &clientAddressLength);
+    inet_ntop(AF_INET, &clientAddress.sin_addr, localIP, sizeof(localIP));
+    int localPort = ntohs(clientAddress.sin_port);
+    char * ipAddress = inet_ntoa(((struct sockaddr_in * ) & clientAddress)->sin_addr);
+    char localAddress[45];
+    char localPortString[25];
+    sprintf(localAddress, "%s", localIP);
+    sprintf(localPortString, "%u", localPort);
+
+    // Prompt user to choose a user name
+    printf("Choose a user name\n");
+    bytesRead = read(0, peerUserName, 10);
+
+    // Initialize the file descriptor sets and register sockets
+    FD_ZERO(&allFileDescriptors);
+    FD_SET(serverSocket, &allFileDescriptors);   // Listen to the index server
+    FD_SET(0, &allFileDescriptors);               // Listen to user input
+    FD_SET(tcpSocketDescriptor, &allFileDescriptors); // Listen to the TCP client server
+
+    numberOfFileDescriptors = 1;
+    for (bytesRead = 0; bytesRead < MAX_CONNECTIONS; bytesRead++)
+        registeredFileTable[bytesRead].fileStatus = -1; // Initialize all file statuses to -1
+
+    // Set up signal handler for SIGINT (Ctrl+C)
+    signalAction.sa_handler = signalHandler;
+    sigemptyset(&signalAction.sa_mask);
+    signalAction.sa_flags = 0;
+    sigaction(SIGINT, &signalAction, NULL);
+
+    // Fork a child process for handling TCP server operations
+    switch ((processId = fork())) {
+    case 0: // Child process for handling TCP connections
+        while (1) {
+            clientAddressLength = sizeof(clientAddress2);
+            newSocketDescriptor = accept(tcpSocketDescriptor, (struct sockaddr * ) & clientAddress2, &clientAddressLength);
+            if (newSocketDescriptor < 0) {
+                fprintf(stderr, "Unable to accept client connection\n");
+                exit(1);
+            }
+
+            // Fork again to handle multiple clients
+            switch (fork()) {
+            case 0: // Child process to handle a specific client download request
+                (void) close(tcpSocketDescriptor);
+                exit(handleServerDownload(newSocketDescriptor));
+            default: // Parent process to continue listening for new clients
+                (void) close(newSocketDescriptor);
+                break;
+            case -1:
+                fprintf(stderr, "Error creating a new process\n");
+            }
+        }
+    default:
+        // Main loop for handling user commands
+        while (1) {
+            printf("Enter command:\n");
+
+            // Copy file descriptors for select
+            memcpy(&readFileDescriptors, &allFileDescriptors, sizeof(readFileDescriptors));
+            if (select(numberOfFileDescriptors, &readFileDescriptors, NULL, NULL, NULL) == -1) {
+                printf("select error: %s\n", strerror(errno));
+                exit(1);
+            }
+
+            // Check if input is from the user
+            if (FD_ISSET(0, &readFileDescriptors)) {
+                char command[40];
+                bytesRead = read(0, command, 40);
+
+                // Command help option
+                if (command[0] == '?') {
+                    printf("R - Register content; T - Deregister content; L - List local content\n");
+                    printf("D - Download content; O - List online content; Q - Quit\n\n");
+                    continue;
+                }
+
+                // Register content
+                if (command[0] == 'R') {
+                    registrationPDU.messageType = 'R';
+                    printf("Enter content name:\n");
+                    char contentName[20];
+                    bytesRead = read(0, contentName, 20);
+                    peerUserName[strcspn(peerUserName, "\n")] = 0;
+                    contentName[strcspn(contentName, "\n")] = 0;
+
+                    if (fopen(contentName, "r") == NULL) {
+                        printf("File does not exist!\n");
+                        continue;
+                    }
+
+                    // Send registration request with username, content name, and port number
+                    strcpy(dataToSend, peerUserName);
+                    strcat(dataToSend, ";");
+                    strcat(dataToSend, contentName);
+                    strcat(dataToSend, ";");
+                    sprintf(dataToSend + strlen(dataToSend), "%d", localPort);
+
+                    // Send registration PDU
+                    registerContent(serverSocket, dataToSend);
+                }
+            }
+        }
+    }
 }
 
-int main(int argc, char *argv[])
-{
+// Register content on the server
+void registerContent(int socket, char * data) {
+    int sendResult;
+    sendResult = send(socket, data, strlen(data), 0); // Send registration message
+    if (sendResult < 0) {
+        printf("Error sending registration message\n");
+        exit(1);
+    }
+}
+
+// Handle server download request
+int handleServerDownload(int socket) {
+    return 0; // Placeholder function, implement your logic
+}
+
+// Reap child processes after handling them
+void childProcessReaper(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+// Exit the program
+void exitProgram(int signal) {
+    exit(0);
+}
+
+// Handle SIGINT signal (Ctrl+C)
+void signalHandler() {
+    printf("\nExiting program...\n");
     exit(0);
 }
